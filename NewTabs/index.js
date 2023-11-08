96f947ead297f18c605aa9b097f76e3b696a3f46
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-11-08 12:06:43 -0500

    Completed exercise on 3-11

--- NewTabs/index.js
+++ NewTabs/index.js
@@ -4,11 +4,21 @@ window.onload = function () {
     const googleBtn = document.getElementById("googleBtn");
     const w3SchoolsBtn = document.getElementById("w3SchoolsBtn");
     const imagesBtn = document.getElementById("imagesBtn");
+    const imageGroup = document.getElementsByClassName("imageGroup");
 
     googleBtn.onclick = googleBtnClicked;
     w3SchoolsBtn.onclick = w3SchoolsBtnClicked;
     imagesBtn.onclick = imagesBtnClicked;
+    
+    let imageGroupsStyle = imageGroup.length
+    for (let i = 0; i < imageGroupsStyle; i++) {
+        imageGroup[i].style.border = "2px solid red";
+    }
 
+    const imagesWithoutAlt = document.querySelectorAll("img:not([alt])");
+    imagesWithoutAlt.forEach(function (image) {
+        image.alt = "graffiti image";
+    });
 };
 
 function googleBtnClicked() {
